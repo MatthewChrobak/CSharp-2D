2982b4a117dfaead4339890c7edc2f77f430843e
MatthewChrobak <ijustgoton@hotmail.com>
2015-03-09 02:47:45 -0400

    Server - Compression
    I started working on the Server.IO namespace. I simplified a few
    compression methods, and added a few new ones.
    
    I also added an event-hook to pressing CTRL + C which would properly
    destroy the server.

---
+++ Server/Server/IO/Compression.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using System.IO.Compression;
+
+namespace Server.IO
+{
+    public static class Compression
+    {
+        public static void CompressDirectory(string directory, string zipFile) {
+            // Check if the directory exists.
+            if (Directory.Exists(directory)) {
+                ZipFile.CreateFromDirectory(directory, zipFile, CompressionLevel.Optimal, false);
+            }
+        }
+
+        public static void DecompressDirectory(string zipFile, string directory) {
+            
+            // If the directory does not exist, creat it.
+            if (!Directory.Exists(directory)) {
+                Directory.CreateDirectory(directory);
+            }
+
+            // Extract the zipped file's contents to the directory.
+            ZipFile.ExtractToDirectory(zipFile, directory);
+        }
+
+
+        public static void Compress(string file) {
+
+            // Make sure that the file exists.
+            if (!File.Exists(file)) {
+                return;
+            }
+
+            // Open the filestream of the file we want to compress.
+            using (var fs = new FileInfo(file).OpenRead()) {
+                // Create a new filestream with the .zip file extention.
+                using (var newFs = new FileStream(file + ".zip", FileMode.OpenOrCreate)) {
+                    // Don't exactly understand this bit, but it works gosh darn it.
+                    using (var compressedFs = new GZipStream(newFs, CompressionMode.Compress)) {
+                        // Copy whatever we did on the previous like to the original filestream.
+                        fs.CopyTo(compressedFs);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Server/Server/Server.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+
+namespace Server
+{
+    public static class Server
+    {
+        // Public accessors
+
+        // Global values
+        public static string StartupPath;
+
+        private static void Main(string[] args) {
+            // Set the startup path of the application.
+            StartupPath = AppDomain.CurrentDomain.BaseDirectory;
+
+            // Check the folders and files in the system.
+
+            // Load the game data.
+
+            // Start the network.
+
+            // Set up the Destroy Server event handlers.
+            Console.WriteLine("[IMPORTANT INFORMATION] : ");
+            Console.WriteLine("------------------------------------------------------------------------------");
+            Console.WriteLine("Remember to turn off the server by pressing [CTRL + C] or [CTRL + BREAK].");
+            Console.WriteLine("If you do not, all online player's data will NOT be saved.");
+            Console.WriteLine("------------------------------------------------------------------------------");
+            Console.CancelKeyPress += (s, e) => {
+                Server.Destroy();
+            };
+
+            // Start the gameloop.
+            Server.GameLoop();
+        }
+
+        private static void Destroy() {
+            Environment.Exit(1);
+        }
+
+        private static void GameLoop() {
+            int tick = 0, tmr10000 = 0;
+
+            while (true) {
+                tick = Environment.TickCount;
+
+                if (tmr10000 < tick) {
+                    tmr10000 = tick + 10000;
+                }
+            }
+        }
+    }
+}
