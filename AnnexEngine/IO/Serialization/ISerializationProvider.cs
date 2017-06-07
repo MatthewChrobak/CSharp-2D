5ebe474fddd022a9f6113e85fa91948b46ffcbb2
Matthew Chrobak <matthewchrobak@hotmail.com>
2017-06-07 02:49:25 -0400

    [Unfinished] Serialization Development

---
+++ AnnexEngine/IO/Serialization/ISerializationProvider.cs
@@ -0,0 +1,10 @@
+﻿using System.IO;
+
+namespace AnnexEngine.IO.Serialization
+{
+    public interface ISerializationProvider
+    {
+        object Deserialize(Stream stream);
+        void Serialize(Stream stream, object o);
+    }
+}
---
+++ AnnexEngine/IO/Serialization/XmlSerialization.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace AnnexEngine.IO.Serialization
+{
+    public class XmlSerializer : ISerializationProvider
+    {
+        private System.Xml.Serialization.XmlSerializer _xml;
+
+        public XmlSerializer(Type type)
+        {
+            this._xml = new System.Xml.Serialization.XmlSerializer(type);
+        }
+
+        public object Deserialize(Stream stream)
+        {
+            return this._xml.Deserialize(stream);
+        }
+
+        public void Serialize(Stream stream, object o)
+        {
+            this._xml.Serialize(stream, o);
+        }
+    }
+
+    public static partial class StreamSerializationExtensions
+    {
+        public static void SerializeToXML<T>(this Stream stream, Type type, T instance)
+        {
+            new XmlSerializer(type).Serialize(stream, instance);
+        }
+
+        public static T DeserializeFromXML<T>(this Stream stream, Type type)
+        {
+            return (T)new XmlSerializer(type).Deserialize(stream);
+        }
+    }
+}
