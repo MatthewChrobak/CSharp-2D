d676870d8013cef8d41fe5a836d0616688a4b21e
Matthew Chrobak <matthewchrobak@hotmail.com>
2017-06-07 19:18:15 -0400

    Basic Object Serialization

---
+++ AnnexEngine/IO/Serialization/BinarySerialization.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace AnnexEngine.IO.Serialization
+{
+    public static partial class StreamSerializationExtensions
+    {
+        public static void SerializeToBinary<T>(this Stream stream, T instance)
+        {
+            new BinaryFormatter().Serialize(stream, instance);
+        }
+
+        public static T DeserializeFromBinary<T>(this Stream stream)
+        {
+            return (T)new BinaryFormatter().Deserialize(stream);
+        }
+    }
+}
--- AnnexEngine/IO/Serialization/XmlSerialization.cs
+++ AnnexEngine/IO/Serialization/XmlSerialization.cs
@@ -1,38 +1,18 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
+using System.Xml.Serialization;
 
 namespace AnnexEngine.IO.Serialization
 {
-    public class XmlSerializer : ISerializationProvider
-    {
-        private System.Xml.Serialization.XmlSerializer _xml;
-
-        public XmlSerializer(Type type)
-        {
-            this._xml = new System.Xml.Serialization.XmlSerializer(type);
-        }
-
-        public object Deserialize(Stream stream)
-        {
-            return this._xml.Deserialize(stream);
-        }
-
-        public void Serialize(Stream stream, object o)
-        {
-            this._xml.Serialize(stream, o);
-        }
-    }
-
     public static partial class StreamSerializationExtensions
     {
-        public static void SerializeToXML<T>(this Stream stream, Type type, T instance)
+        public static void SerializeToXML<T>(this Stream stream, T instance)
         {
-            new XmlSerializer(type).Serialize(stream, instance);
+            new XmlSerializer(typeof(T)).Serialize(stream, instance);
         }
 
-        public static T DeserializeFromXML<T>(this Stream stream, Type type)
+        public static T DeserializeFromXML<T>(this Stream stream)
         {
-            return (T)new XmlSerializer(type).Deserialize(stream);
+            return (T)new XmlSerializer(typeof(T)).Deserialize(stream);
         }
     }
 }
