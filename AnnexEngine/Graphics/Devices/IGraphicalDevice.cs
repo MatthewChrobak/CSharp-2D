0d27cc2a312bface52066b893a551b31a1bcce5b
Matthew Chrobak <matthewchrobak@hotmail.com>
2017-06-27 23:11:17 -0400

    Slightly changed documentation of IGraphicalDevice

--- AnnexEngine/Graphics/Devices/IGraphicalDevice.cs
+++ AnnexEngine/Graphics/Devices/IGraphicalDevice.cs
@@ -3,19 +3,19 @@
 namespace AnnexEngine.Graphics.Devices
 {
     /// <summary>
-    /// A graphical object that is capable of drawing surfaces and text on itself.
+    /// A graphical object that is capable of drawing surfaces and text onto itself.
     /// </summary>
     public interface IGraphicalDevice : IDrawable
     {
         /// <summary>
-        /// Draws a surface with the given context on the graphical device.
+        /// Draws a surface with the given context onto the graphical device.
         /// </summary>
         /// <param name="surfaceName">The name of the surface to be drawn.</param>
         /// <param name="context">The context to be applied to the surface.</param>
         void DrawSurface(string surfaceName, SurfaceContext context);
 
         /// <summary>
-        /// Draws text with the given context on the graphical device.
+        /// Draws text with the given context onto the graphical device.
         /// </summary>
         /// <param name="text">The text to be drawn.</param>
         /// <param name="context">The context to be applied to the text.</param>
